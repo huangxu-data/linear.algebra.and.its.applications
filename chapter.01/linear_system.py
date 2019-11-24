9ad4401c85c126b1836ed12b88b51093cd88840d
Huang Xu <xuhuang601@gmail.com>
2019-11-24 21:31:34 +0800

    add section 1.03 notes

--- chapter.01/linear_system.py
+++ chapter.01/linear_system.py
@@ -14,7 +14,7 @@ def draw_lines(augmented, start=-4,
     plt.show()
 
 
-def eliminate_forward(augmented): 
+def eliminate_forward(augmented, verbose=False): 
     """消元法的前向过程"""
     A = np.asarray(augmented, dtype=np.float64)
     # row number of the last row
@@ -35,14 +35,18 @@ def eliminate_forward(augmented):
         pivots.append((i, j))
         i += 1
         j += 1
+        if verbose:
+            print(A)
     return A, pivots
 
 
-def eliminate_backward(simplified, pivots):
+def eliminate_backward(simplified, pivots, verbose=False):
     """消元法的后向过程."""
     A = np.asarray(simplified)
     for i, j in reversed(pivots):
         A[:i, j:] = A[:i, j:] - A[[i], j:] * A[:i, [j]] 
+        if verbose:
+            print(A)
     return A
 
 
@@ -75,6 +79,83 @@ def eliminate(augmented):
     print("-"*30)
 
 
+class Vector:
+    """用Python实现一个向量类，不使用Numpy."""
+    def __init__(self, data:(list, tuple)):
+        """data应该是一个列表或元组"""
+        self._data = tuple(data)
+    
+    def __getitem__(self, index):
+        """使下标语法和迭代生效：即x[i]有效."""
+        return self._data[index]
+    
+    def __radd__(self, other):
+        """标量或向量other与向量self相加，等价于self+other."""
+        return self + other
+    
+    def __add__(self, other):
+        """
+        加法，返回元素相加后的Vector.
+        
+        若other是Vector，list或tuple，返回元素相加的结果；
+        若other 是标量，则扩展为值全为other的向量。
+        与书不一致，与Numpy一致。
+        """ 
+        if isinstance(other, (float, int)):
+            return Vector([other + i for i in self])
+        if isinstance(other, (Vector, list, tuple)):
+            return Vector(left + right for left, right 
+                         in zip(self, other))
+        raise ValueError("the parameter is in wrong type")
+    
+    def __sub__(self, other):
+        """
+        减法，返回元素相加后的Vector.
+        
+        若other是Vector，list或tuple，返回元素相减的结果；
+        若other 是标量，则扩展为值全为other的向量。
+        与书不一致，与Numpy一致。
+        """ 
+        if isinstance(other, (float, int)):
+            return Vector(i - other for i in self)
+        if isinstance(other, (Vector, list, tuple)):
+            return Vector(i - j for i, j 
+                          in zip(self, other))
+        raise ValueError("the parameter is in wrong type")
+        
+    def __rsub__(self, other):
+        """标量或列表other与向量self相减，等价于self-other."""
+        return other - self
+    
+    def __mul__(self, other):
+        """
+        乘法，返回元素相乘后的Vector.
+        
+        若other 是标量，则为标量乘向量。
+        若other是Vector，list或tuple，返回元素相乘的结果；
+        与书不一致，与Numpy一致。
+        """ 
+        if isinstance(other, (float, int)):
+            return Vector(other * i for i in self)
+        if isinstance(other, (Vector, list, tuple)):
+            return Vector(i * j for i, j in zip(self, other))
+        raise ValueError("the parameter is in wrong type")
+        
+    def __rmul__(self, other):
+        return self * other 
+    
+    def __matmul__(self, other):
+        if isinstance(other, (Vector, list, tuple)):
+            return sum(i * j for i, j in zip(self, other))
+        raise ValueError("the parameter is in wrong type")
+    
+    def __rmatmul__(self, other):
+        return self @ other
+
+    def __repr__(self):
+        return "[" + "\n ".join(str(x) for x in self._data) + "]"
+
+
 def draw_figure1():
     draw_lines([[1, -2, -1], 
                 [-1, 3, 3]])
@@ -83,7 +164,7 @@ def draw_figure1():
     draw_lines([[1, -2, -1], 
                 [-1, 2, 1]])
 
-                
+
 def section_01():
     # draw_figure1()
     # 1.1 example 1
